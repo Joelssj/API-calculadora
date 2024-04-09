f0fe59e952427b6b2656d0da9f90be54bbdc630b
JOEL DE JESUS LOPEZ RUIZ <joelssj93@gmail.com>
2024-04-09 11:02:33 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,77 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import moo from "moo";
+
+const app = express();
+const PORT = 3010;
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+let lexer = moo.compile({
+  WS: /[ \t]+/,
+  float: /0\.[0-9]+|[1-9][0-9]*\.[0-9]+|\.[0-9]+/,
+  integer: /[0-9]+/,
+  plus: "+",
+  minus: "-",
+  times: "*",
+  div: "/",
+  lparen: "(",
+  rparen: ")",
+  NL: { match: /\n/, lineBreaks: true },
+});
+
+app.post("/calculadora", (req, res) => {
+  try {
+    const { expresion } = req.body;
+
+    lexer.reset(expresion);
+    let token;
+    let resultado = [];
+    let linea = 1;
+    let prevToken = null;
+    while ((token = lexer.next())) {
+      if (token.type !== "NL" && token.type !== "WS") {
+        if (
+          prevToken &&
+          (prevToken.type === "integer" || prevToken.type === "rparen") &&
+          (token.type === "integer" || token.type === "lparen")
+        ) {
+          resultado.push({
+            linea,
+            tipo: "times",
+            valor: "*",
+            posicion: token.col - 1,
+          });
+          token.col++;
+        }
+        resultado.push({
+          linea,
+          tipo: token.type,
+          valor: token.value,
+          posicion: token.col - 1,
+        });
+      }
+      if (token.type === "NL") {
+        linea++;
+      }
+      prevToken = token;
+    }
+
+    res.send({ resultado });
+  } catch (error) {
+    res.status(500).send({ error: error.toString() });
+    console.error(error);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send(`API running on port ${PORT}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
